13ea2b60d10913d803df2c5a68d369ae0727fe93
BessyPBabbu <bessypbabu@gmail.com>
2026-04-13 13:52:34 +0530

    fix: add CSRF_TRUSTED_ORIGINS setting

--- marlo_cms/settings.py
+++ marlo_cms/settings.py
@@ -36,6 +36,11 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.environ.get(
+    'CSRF_TRUSTED_ORIGINS',
+    'http://localhost:8000'
+).split(',')
+
 
 # Application definition
 
