1f3c6930af74c97c5e21729504ac15f3869c2c0d
BessyPBabbu <bessypbabu@gmail.com>
2026-04-13 11:29:59 +0530

    fix: remove nixpacks, fix build commands

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
+
+# Use the venv Python that Railway creates at /app/.venv
+export PATH="/app/.venv/bin:$PATH"
+
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
